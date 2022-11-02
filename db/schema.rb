ac875ea579c1195e8fda249587767e0fe16f9c43
vipin <champvip32@gmail.com>
2022-11-02 13:03:14 +0530

    implemented comment functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_172249) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_063212) do
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
